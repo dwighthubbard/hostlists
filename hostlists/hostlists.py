a1dde46ed0d310009cbad155d5d2fd2eda952b5f
dhubbard <dhubbard@yahoo-inc.com>
2013-03-04 16:14:30 -0800

    Code cleanup

--- hostlists/hostlists.py
+++ hostlists/hostlists.py
@@ -36,45 +36,48 @@ import re
 import socket
 
 # Global plugin cache so we don't constantly reload the plugin modules
-global_plugins={}
+global_plugins = { }
 
 # A list of operators we use for set options
-SET_OPERATORS=['-']
+SET_OPERATORS = ['-']
 
 # Config file
-CONF_FILE=os.path.expanduser('~/.hostlists.conf')
+CONF_FILE = os.path.expanduser('~/.hostlists.conf')
+
 
 def _get_plugins():
     """ Find all the hostlists plugins """
-    plugins=global_plugins
-    pluginlist=[]
-    plugin_path=['/usr/lib/hostlists','/home/y/lib/hostlists']+sys.path
+    plugins = global_plugins
+    pluginlist = []
+    plugin_path = ['/usr/lib/hostlists', '/home/y/lib/hostlists'] + sys.path
     for directory in plugin_path:
-        if os.path.isdir(os.path.join(directory,'plugins')):
-            templist=os.listdir(os.path.join(directory,'plugins'))
+        if os.path.isdir(os.path.join(directory, 'plugins')):
+            templist = os.listdir(os.path.join(directory, 'plugins'))
             for item in templist:
-                pluginlist.append(os.path.join(os.path.join(directory,'plugins'),item))
+                pluginlist.append(os.path.join(os.path.join(directory, 'plugins'), item))
     pluginlist.sort()
     # Create a dict mapping the plugin name to the plugin method
     for item in pluginlist:
         if item.endswith('.py'):
             #mod=__import__(item[:-3])
             try:
-                mod=imp.load_module('hostlists_plugins_%s'% os.path.basename(item[:-3]),open(item),item,('.py','r',imp.PY_SOURCE))
-                names=mod.name()
+                mod = imp.load_module('hostlists_plugins_%s' % os.path.basename(item[:-3]), open(item), item,
+                                      ('.py', 'r', imp.PY_SOURCE))
+                names = mod.name()
                 if type(names) is str:
-                    names=[names]
+                    names = [names]
                 for name in names:
                     if name not in plugins.keys():
-                        plugins[name.lower()]=mod
+                        plugins[name.lower()] = mod
             except:
                 # Error in module import, probably a plugin bug
                 pass
     return plugins
 
+
 def get_setting(key):
     try:
-        settings=json.load(open(CONF_FILE,'r'))
+        settings = json.load(open(CONF_FILE, 'r'))
     except IOError:
         # Couldn't open the settings file
         #print 'No such conf file'
@@ -82,8 +85,9 @@ def get_setting(key):
     if key in settings.keys():
         return settings[key]
     return None
-         
-def expand(range_list,onepass=False):
+
+
+def expand(range_list, onepass = False):
     """ 
     Expand a list of lists and set operators into a final host lists 
     >>> hostlists.expand(['foo[01-10]','-','foo[04-06]'])
@@ -91,83 +95,89 @@ def expand(range_list,onepass=False):
     >>> 
     """
     if type(range_list) is str:
-      range_list=[range_list]
-    new_list=[]
-    set1=None
-    operation=None
+        range_list = [range_list]
+    new_list = []
+    set1 = None
+    operation = None
     for item in range_list:
         if set1 and operation:
-            set2=expand_item(item)
+            set2 = expand_item(item)
             new_list.append(list(set(set1).difference(set(set2))))
-            set1=None
-            set2=None
-            operation=None
+            set1 = None
+            set2 = None
+            operation = None
         elif item in SET_OPERATORS and len(new_list):
-            set1=new_list.pop()
-            operation=item
+            set1 = new_list.pop()
+            operation = item
         else:
-            expanded_item=expand_item(item,onepass=onepass)
+            expanded_item = expand_item(item, onepass = onepass)
             new_list.append(expanded_item)
-    new_list2=[]
+    new_list2 = []
     for item in new_list:
-        new_list2+=item
+        new_list2 += item
     return new_list2
 
+
 def multiple_names(plugin):
-    plugins=_get_plugins()
+    plugins = _get_plugins()
     count = 0
     for item in plugins.keys():
-        if plugins[item]==plugin:
+        if plugins[item] == plugin:
             count += 1
     if count > 1:
         return True
     else:
         return False
-        
-def expand_item(range_list,onepass=False):
+
+
+def expand_item(range_list, onepass = False):
     """ Expand a list of plugin:parameters into a list of hosts """
     #range_list=list(range_list)      
     # Find all the host list plugins
     if type(range_list) is str:
-      range_list=[range_list]
-    plugins=_get_plugins()
-    
+        range_list = [range_list]
+    plugins = _get_plugins()
+
     # Iterate through our list
-    newlist=[]
-    found_plugin=False
+    newlist = []
+    found_plugin = False
     for item in range_list:
         # Is the item a plugin
-        temp=item.split(':')
+        temp = item.split(':')
         if len(temp) > 1:
-            plugin=temp[0].lower()
+            plugin = temp[0].lower()
             # Do we have a plugin that matches the passed plugin
             if plugin in plugins.keys():
                 # Call the plugin
-                item=None
+                item = None
                 if multiple_names(plugins[plugin]):
-                    newlist+=plugins[plugin].expand(':'.join(temp[1:]).strip(':'),name=plugin)
+                    newlist += plugins[plugin].expand(':'.join(temp[1:]).strip(':'), name = plugin)
                 else:
-                    newlist+=plugins[plugin].expand(':'.join(temp[1:]).strip(':'))
-                found_plugin=True
+                    newlist += plugins[plugin].expand(':'.join(temp[1:]).strip(':'))
+                found_plugin = True
             else:
-                print >>sys.stderr, 'plugin',plugin,'not found',plugins.keys()
+                print >> sys.stderr, 'plugin', plugin, 'not found', plugins.keys()
         else:
             # Default to running through the range plugin
-            item=None
-            newlist+=plugins['range'].expand(temp[0])
+            item = None
+            newlist += plugins['range'].expand(temp[0])
         if item:
             newlist.append(item)
-    # Recurse back through ourselves incase a plugin returns a value that needs to be parsed
+        # Recurse back through ourselves incase a plugin returns a value that needs to be parsed
     # by another plugin.  For example a dns resource that has an address that points to a
     # load balancer vip that may container a number of hosts that need to be looked up via
     # the load_balancer plugin.
     if found_plugin and not onepass:
-        newlist=expand_item(newlist)
+        newlist = expand_item(newlist)
     return newlist
 
-def multikeysort(items, columns):  
+
+def multikeysort(items, columns):
     from operator import itemgetter
-    comparers = [ ((itemgetter(col[1:].strip()), -1) if col.startswith('-') else (itemgetter(col.strip()), 1)) for col in columns]
+
+    comparers = [((itemgetter(col[1:].strip()), -1) if col.startswith('-') else (itemgetter(col.strip()), 1)) for col in
+                 columns]
+
     def comparer(left, right):
         for fn, mult in comparers:
             try:
@@ -178,7 +188,8 @@ def multikeysort(items, columns):
                 return mult * result
         else:
             return 0
-    return sorted(items, cmp=comparer)
+
+    return sorted(items, cmp = comparer)
 
 
 def compress(hostnames):
@@ -187,75 +198,76 @@ def compress(hostnames):
     # really compress at all.  This functionality isn't really needed by
     # sshmap to function.
     hostnames.sort()
-    prev_dict={'prefix':"",'suffix':'','number':0}
-    items=[]
-    items_block=[]
-    new_hosts=[]
+    prev_dict = { 'prefix': "", 'suffix': '', 'number': 0 }
+    items = []
+    items_block = []
+    new_hosts = []
     for host in hostnames:
         #print re.match(r"([^0-9]+)(\d+)(.+).?",sys.argv[1]).groups()
         try:
-            parsed_dict=re.match(r"(?P<prefix>[^0-9]+)(?P<number>\d+)(?P<suffix>.+).?",host).groupdict()
+            parsed_dict = re.match(r"(?P<prefix>[^0-9]+)(?P<number>\d+)(?P<suffix>.+).?", host).groupdict()
             # To generate the range we need the entries sorted numerically but to ensure we don't
             # loose any leading 0s we don't want to replace the number parameter that is a string
             # with the leading 0s.
-            parsed_dict['number_int']=int(parsed_dict['number'])
+            parsed_dict['number_int'] = int(parsed_dict['number'])
             new_hosts.append(parsed_dict)
         except AttributeError:
             if '.' not in host:
-                host+='.'
-                parsed_dict={'host':compress([host])[0].strip('.')}
+                host += '.'
+                parsed_dict = { 'host': compress([host])[0].strip('.') }
             else:
-                parsed_dict={'host':host}
+                parsed_dict = { 'host': host }
             new_hosts.append(parsed_dict)
-    new_hosts=multikeysort(new_hosts,['prefix','number_int'])
-    for parsed_dict in new_hosts: 
-        if 'host' in parsed_dict.keys() or parsed_dict['prefix']!=prev_dict['prefix'] or parsed_dict['suffix'] != prev_dict['suffix'] or int(parsed_dict['number']) != int(prev_dict['number'])+1:
+    new_hosts = multikeysort(new_hosts, ['prefix', 'number_int'])
+    for parsed_dict in new_hosts:
+        if 'host' in parsed_dict.keys() or parsed_dict['prefix'] != prev_dict['prefix'] or parsed_dict['suffix'] != prev_dict['suffix'] or int(parsed_dict['number']) != int(prev_dict['number']) + 1:
             if len(items_block):
                 items.append(items_block)
-            items_block=[parsed_dict]
+            items_block = [parsed_dict]
         else:
             items_block.append(parsed_dict)
-        prev_dict=parsed_dict
+        prev_dict = parsed_dict
     items.append(items_block)
-    result=[]
+    result = []
     for item in items:
         if len(item):
             if len(item) == 1 and 'host' in item[0].keys():
                 result.append(item[0]['host'])
             elif len(item) == 1:
-                result.append('%s%s%s' % (item[0]['prefix'],item[0]['number'],item[0]['suffix']))
+                result.append('%s%s%s' % (item[0]['prefix'], item[0]['number'], item[0]['suffix']))
             else:
-                result.append('%s[%s-%s]%s' % (item[0]['prefix'],item[0]['number'],item[-1]['number'],item[0]['suffix']))
+                result.append('%s[%s-%s]%s' % (item[0]['prefix'], item[0]['number'], item[-1]['number'], item[0]['suffix']))
     return result
 
-        
+
 def range_split(range):
     """ Split up a range string, this needs to separate comma separated
     items unless they are within square brackets and split out set operations
     as separate items."""
-    in_brackets=False
-    current=""
-    result_list=[]
+    in_brackets = False
+    current = ""
+    result_list = []
     for c in range:
         if c in ['[']:
-            in_brackets=True
+            in_brackets = True
         if c in [']']:
-            in_brackets=False
-        if not in_brackets and c==',':
+            in_brackets = False
+        if not in_brackets and c == ',':
             result_list.append(current)
-            current=""
-        elif not in_brackets and c=='-':
+            current = ""
+        elif not in_brackets and c == '-':
             result_list.append(current)
             result_list.append('-')
-            current=""
+            current = ""
         elif not in_brackets and c in [','] and len(current) == 0:
             pass
         else:
-            current+=c
+            current += c
     if len(current):
         result_list.append(current)
     return result_list
-    
+
+
 if __name__ == "__main__":
     # This has been moved to the hostlist script
     pass
